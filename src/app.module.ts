ad644f4803f6bfedf02724c5792f9b63f5805fe5
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-25 17:05:41 -0500

    chore: reorganize imports in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { LogsModule } from './_logs/logs.module';
 import { AuthModule } from './auth/auth.module';
 import { DocumentsModule } from './documents/documents.module';
 import { FilesModule } from './files/files.module';
-import { LogsModule } from './logs/logs.module';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 import { NatsModule } from './transports/nats.module';
