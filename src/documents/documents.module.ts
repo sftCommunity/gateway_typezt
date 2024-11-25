426469ed469a924f5087606ce13577e3686f3e11
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-25 17:08:25 -0500

    feat: add NatsModule and DocumentsController to DocumentsModule

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -1,3 +1,8 @@
 import { Module } from '@nestjs/common';
-@Module({})
+import { NatsModule } from 'src/transports/nats.module';
+import { DocumentsController } from './documents.controller';
+@Module({
+  controllers: [DocumentsController],
+  imports: [NatsModule],
+})
 export class DocumentsModule {}
