cae67834812c9bf50c01aac8232683e27d2fa7db
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-25 17:04:27 -0500

    feat: add documents and logs modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { DocumentsModule } from './documents/documents.module';
+import { FilesModule } from './files/files.module';
+import { LogsModule } from './logs/logs.module';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 import { NatsModule } from './transports/nats.module';
-import { AuthModule } from './auth/auth.module';
-import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule, NatsModule, AuthModule, FilesModule],
+  imports: [
+    ProductsModule,
+    OrdersModule,
+    NatsModule,
+    AuthModule,
+    FilesModule,
+    DocumentsModule,
+    LogsModule,
+  ],
   controllers: [],
   providers: [],
 })
